0b2c91f893441127a3f1be2196d18a05c0f4b1bd
Odilon Junior <odilon.junior93@gmail.com>
2019-03-15 18:57:25 -0300

    Primeiro Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Flask do Bootcamp"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=8080)
